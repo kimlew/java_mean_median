80b65e0dee8ebc1e359cdcc45cf1ab188434179a
Kim Lew <kimlew451@gmail.com>
2017-07-25 10:25:31 -0600

    Added 3 print statements asking user for year, month, day

--- MeanMedian.java
+++ MeanMedian.java
@@ -6,6 +6,13 @@ public class MeanMedian {
     String[] s = { "", "", "" };
     
     Scanner in = new Scanner(System.in);
+    
+    System.out.println("Emter a year (2012-2017)");
+    
+    System.out.println("Enter a month (01-12)");
+    
+    System.out.println("Enter a day (01-31)");
+    
   }
 
 } // End of: class
