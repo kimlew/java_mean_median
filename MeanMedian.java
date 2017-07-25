8d075c0b7e84cfde897b64175210d8aa934090c8
Kim Lew <kimlew451@gmail.com>
2017-07-25 10:22:53 -0600

    Created scanner object for input

--- MeanMedian.java
+++ MeanMedian.java
@@ -4,6 +4,8 @@ public class MeanMedian {
 
   public static void main(String[] args) throws IOException {
     String[] s = { "", "", "" };
+    
+    Scanner in = new Scanner(System.in);
   }
 
 } // End of: class
