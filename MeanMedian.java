6b0a47ee3f7a83274c0537da4ad5b2e32cd30c08
Kim Lew <kimlew451@gmail.com>
2017-07-25 10:21:21 -0600

    Added array, s, with 3 empty strings

--- MeanMedian.java
+++ MeanMedian.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 public class MeanMedian {
 
   public static void main(String[] args) throws IOException {
-  
+    String[] s = { "", "", "" };
   }
 
 } // End of: class
