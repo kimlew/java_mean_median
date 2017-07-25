6d270b078e4a0994a123203a596880e56ec14466
Kim Lew <kimlew451@gmail.com>
2017-07-25 10:27:09 -0600

    Assign each of year, month, day to s[0], s[1], s[2]

--- MeanMedian.java
+++ MeanMedian.java
@@ -8,11 +8,13 @@ public class MeanMedian {
     Scanner in = new Scanner(System.in);
     
     System.out.println("Emter a year (2012-2017)");
+    s[0] = in.nextLine();
     
     System.out.println("Enter a month (01-12)");
+    s[1] = in.nextLine();
     
     System.out.println("Enter a day (01-31)");
-    
+    s[2] = in.nextLine();
   }
 
 } // End of: class
