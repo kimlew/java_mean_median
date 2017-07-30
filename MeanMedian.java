25ef2d485e38fbd191948f6868da81d79f739a78
Kim Lew <kimlew451@gmail.com>
2017-07-30 09:57:42 -0600

    Called getData(null, null) & assigned to ArrayList distRunData

--- MeanMedian.java
+++ MeanMedian.java
@@ -6,8 +6,10 @@ import java.util.ArrayList;
 public class MeanMedian {
 
   public static void main(String[] args) throws IOException {
+    
     String[] s = { "", "", "" };
     
+    /*
     Scanner in = new Scanner(System.in);
     
     System.out.println("Enter a year (2017)");
@@ -20,51 +22,54 @@ public class MeanMedian {
     s[2] = in.nextLine();
     
     in.close();
+    */
+    ArrayList distRunData = getData(null, null);
+    System.out.println(distRunData);
+    
   } // End of: main()
   
-  // Gets data and returns it in an ArrayList.
+  // Gets data  from a file and returns it in an ArrayList.
   public static ArrayList<Double> getData(String type, String[] s)
     throws NumberFormatException, IOException {
     
     /*URL dataSource = new URL("http://lpo.dt.navy.mil/data/DM/" +
       s[0] + s[0] + "_" + s[1] + "_" + s[2] + "/" + type);
     */
-    // TODO: Add in correct syntax: String?? dataSource = "distances_run.txt";
+    // TODO: Add in correct syntax:
    
     // TODO: Refer to filename, not URL dataSource
-    /* stream - is Java's term for an external data source where 
-      the data is read 1 item at a time, rather than downloaded in 
+    /* stream - Java's term for an external data source where the
+      data is read 1 item at a time, rather than downloaded in 
       a single chunk. 
       Want: To read 1 line of this file at a time, so use this stream .
     */
-    BufferedReader data = new BufferedReader(new InputStreamReader(dataSource.openStream)));
+    String dataSource = "distances_run.txt";
+    BufferedReader data = new BufferedReader(new FileReader(dataSource));
+
+    //B4: BufferedReader data = new BufferedReader(new InputStreamReader(dataSource.openStream)));
     
     // Define distRunData using an array list.
     // Easiest way to store data - in an array list of doubles
     ArrayList<Double> distRunData = new ArrayList<Double>();
     String inputLine; // Define a string, inputLine - to download data into list
     
-    /*  while statement - reads lines from the data 1-by-1 until
-        there are no more lines to read. 
-      
-      Within this while statement, the code to execute is 
-      distData.add, Double, parseDouble and inputLine. 
-      This takes a string from the input line and treats it as a 
-      double since in this case, it is, a double, rather than a 
-      string.
-
-      Remember: The data on the server is NOT currently a number.
-      It's a date, a time, and a number. The actual data starts on 
-      the 20th character of each line, so add the function 
-      substring 20 - to get rid of all the extraneous information.
-
- */
+    // while statement - reads lines from the data 1-by-1 until
+    // there are no more lines to read
+        
     while ( (inputLine = data.readLine()) != null ) {
-      
+      // Take a string from the input line and treat it as a double
+      // - since in this case, it is, a double, rather than a string
+      // Remember: The data on the server is NOT currently a number.
+      // - is a date, a time, and a number. 
+      // - actual data starts on the 20th character of each line
+      // - so add function substring(20) - gets rid of all extraneous info
+
+      distRunData.add(Double.parseDouble(inputLine.substring(20)));
     }
     
     // Close the data stream and return the array list.
-    // Done: Ability to get data from a file.
+    data.close();
+    return distRunData;
     
   } // End of: getData()
 
