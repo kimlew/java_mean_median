43cefb05a89f586bc258d53a9bca36ac403105aa
Kim Lew <kimlew451@gmail.com>
2017-07-30 11:13:38 -0600

    Called getData(s) & imported java.util.Scanner

--- MeanMedian.java
+++ MeanMedian.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MeanMedian {
 
@@ -26,12 +28,18 @@ public class MeanMedian {
     */
     in.close(); // Close scanner
     
-    ArrayList distRunData = getData(null, null);
+    ArrayList distRunData = getData(s); // To test: getData(null)
     System.out.println(distRunData);
     /*
     mac$ javac MeanMedian.java
     mac$ java MeanMedian
     [14.1, 13.2, 13.3, 20.4, 23.5, 13.6, 14.7]
+    
+    mac$ javac MeanMedian.java
+mac$ java MeanMedian
+Enter a year (2017)
+2017
+[14.1, 13.2, 13.3, 20.4, 23.5, 13.6, 14.7]
     */
     
   } // End of: main()
@@ -39,26 +47,37 @@ public class MeanMedian {
   // Gets data from a file and returns it in an ArrayList.
   public static ArrayList<Double> getData(String[] s)
     throws NumberFormatException, IOException {
-    /*
-    get data takes in two inputs, a string called type and then a string array. The string array contains the three values for the day, which we just specified, and the type array will specify whether we want wind gusts, air temperature, or barometric pressure. In the main method we have three lines accessing this method and each one is getting the values for different type of data.
+    /* get data() - takes 1 input, a string array - that contains the 3 values
+    for the day
+    In the main() method: access getData() method - get values distRunData
+Use input - take advantage of structure of given URL 
+- to systematically generate the correct URL for the data based on the input
 
-But how can we actually use this input? Essentially we're going to take advantage of the structure of the given URL to systematically generate the correct URL for the data based on the input. Rather than typing the whole URL out in a string, we can use the following format. The URL is set right up until the date at which point s0 s1 and s2 are used in place of actual numbers, and then the type is specified. Note that I also added in a bunch of slashes and underscores in quotes to match the exact formatting of the URL.
+Rather than typing the whole URL out in a string, use this format:
+- URL is set right up until the date - at which point s0 s1 and s2 are used
+ in place of actual numbers. 
+ Note: added / slashes and _ underscores in quotes - to match the exact 
+ formatting of the URL
 
-It's important to ensure that you do match this formatting exactly, otherwise, the URL will not detect. With this code, we will now take in user input and get data from the external data source, based on that input. Now let's move on to operating on that data
+Important: to match this formatting exactly, or the URL will not detect. 
+With this code, we will now take in user input and get data from the 
+external data source, based on that input.
+
+Next: operating on that data
 
     */
     
-    /*B4: 
+    /*B4: Note: This wind URL is gone - so I cannot use.
     URL dataSource = new URL("http://lpo.dt.navy.mil/data/DM/" +
       s[0] + s[0] + "_" + s[1] + "_" + s[2] + "/" + type);
     BufferedReader data = new BufferedReader(new InputStreamReader(dataSource.openStream)));
     */
    
-    // Note: Refer to filename instead of URL dataSource - since wind URL changed
-    // stream - Java's term for an external data source where the data is read 
+    // Note: Refer to file using file name instead.
+    // stream - Java's term for an external data source where data is read 
     // 1 item at a time, rather than downloaded in a single chunk. 
     // Want: To read 1 line of this file at a time, so use this stream.
-    String dataSource = "distances_run.txt";
+    String dataSource = "distances_run.txt"; // + s[0] + s[0] + "_" 
     BufferedReader data = new BufferedReader(new FileReader(dataSource));
     
     // Define distRunData using an array list - since easiest way to store data
