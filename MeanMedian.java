e01925baadf03659b108d2ca93841ff6fa93296b
Kim Lew <kimlew451@gmail.com>
2017-07-30 10:25:09 -0600

    Edited several comments

--- MeanMedian.java
+++ MeanMedian.java
@@ -25,44 +25,45 @@ public class MeanMedian {
     */
     ArrayList distRunData = getData(null, null);
     System.out.println(distRunData);
+    /*
+    mac$ javac MeanMedian.java
+    mac$ java MeanMedian
+    [14.1, 13.2, 13.3, 20.4, 23.5, 13.6, 14.7]
+    */
     
   } // End of: main()
   
-  // Gets data  from a file and returns it in an ArrayList.
+  // Gets data from a file and returns it in an ArrayList.
   public static ArrayList<Double> getData(String type, String[] s)
     throws NumberFormatException, IOException {
     
-    /*URL dataSource = new URL("http://lpo.dt.navy.mil/data/DM/" +
+    /*B4: 
+    URL dataSource = new URL("http://lpo.dt.navy.mil/data/DM/" +
       s[0] + s[0] + "_" + s[1] + "_" + s[2] + "/" + type);
+    BufferedReader data = new BufferedReader(new InputStreamReader(dataSource.openStream)));
     */
-    // TODO: Add in correct syntax:
    
-    // TODO: Refer to filename, not URL dataSource
-    /* stream - Java's term for an external data source where the
-      data is read 1 item at a time, rather than downloaded in 
-      a single chunk. 
-      Want: To read 1 line of this file at a time, so use this stream .
-    */
+    // Note: Refer to filename instead of URL dataSource - since wind URL changed
+    // stream - Java's term for an external data source where the data is read 
+    // 1 item at a time, rather than downloaded in a single chunk. 
+    // Want: To read 1 line of this file at a time, so use this stream.
     String dataSource = "distances_run.txt";
     BufferedReader data = new BufferedReader(new FileReader(dataSource));
-
-    //B4: BufferedReader data = new BufferedReader(new InputStreamReader(dataSource.openStream)));
     
-    // Define distRunData using an array list.
-    // Easiest way to store data - in an array list of doubles
+    // Define distRunData using an array list - since easiest way to store data
+    // is in array list of doubles
     ArrayList<Double> distRunData = new ArrayList<Double>();
     String inputLine; // Define a string, inputLine - to download data into list
     
-    // while statement - reads lines from the data 1-by-1 until
-    // there are no more lines to read
+    // while statement - reads lines from data 1-by-1 until no more lines to read
         
     while ( (inputLine = data.readLine()) != null ) {
       // Take a string from the input line and treat it as a double
-      // - since in this case, it is, a double, rather than a string
+      // Note: It is a double, rather than a string.
       // Remember: The data on the server is NOT currently a number.
       // - is a date, a time, and a number. 
       // - actual data starts on the 20th character of each line
-      // - so add function substring(20) - gets rid of all extraneous info
+      // - substring(20) - gets ONLY the distance data - NOT year, month, date
 
       distRunData.add(Double.parseDouble(inputLine.substring(20)));
     }
